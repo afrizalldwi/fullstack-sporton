79d7a09620e79e18cab8a877cbf906bd780d79ef
AfrizalDwi22 <afrizalanas22@gmail.com>
2026-01-18 16:00:26 +0700

    cart pop up

---
+++ app/(landing)/components/ui/card-with-header.tsx
@@ -0,0 +1,11 @@
+function CardWithHeader() {
+  return (
+    <div className="bg-white">
+      <div className="px-5 py-4 border-b border-gray-200">
+        <h2 className="font-bold text-lg">Cart Items</h2>
+      </div>
+    </div>
+  );
+}
+
+export default CardWithHeader;
